b6a1992a546eb1eaa8ea8954cc29ee1d0f0afa37
Marcello <marcello.tavares@snet.net.br>
2020-02-03 14:32:28 -0300

    log

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
@@ -15,6 +15,7 @@ namespace stresser
         static int counter = 0;
         static int threads = 0;
         static readonly HttpClient http_client = new HttpClient();
+        static readonly ConcurrentQueue<string> queue_log = new ConcurrentQueue<string>();
 
         static async Task Main(string[] args)
         {
@@ -24,24 +25,33 @@ namespace stresser
             string? p_soap_action = args.Length >= 4 ? args[3] : null;
 
             using var stream_reader = new StreamReader(Console.OpenStandardInput(), Encoding.UTF8, true);
+            using var stream_error = new StreamWriter(Console.OpenStandardError(), new UTF8Encoding(false));
+
             var payload = await stream_reader.ReadToEndAsync();
             var timer = Stopwatch.StartNew();
-            var cts = new CancellationTokenSource();
-            var task_stop = StressStop(cts, p_delay);
-            var tasks = new List<Task>(p_threads);
-            Parallel.For(0, p_threads, x => tasks.Add(StressLoop(cts.Token, p_url, payload, p_soap_action)));
-            Task.WaitAll(tasks.ToArray());
+            var cts_stress = new CancellationTokenSource();
+            var cts_log = new CancellationTokenSource();
+            var task_stop = StressStop(cts_stress, p_delay);
+            var task_log = StressLog(cts_log.Token);
+            var tasks = new ConcurrentBag<Task>();
+            Parallel.For(0, p_threads, x => tasks.Add(StressLoop(cts_stress.Token, p_url, payload, p_soap_action)));
+            await Task.WhenAll(tasks.ToArray());
+            cts_log.Cancel();
+            await Task.WhenAll(task_log);
             double time = timer.ElapsedMilliseconds;
-            Console.WriteLine($"COUNT:{counter} THREADS:{threads} TIME:{Math.Round(time / 1000, 2)}s SPEED:{Math.Round((double)counter / (time / 1000), 2)}/s");
+            await stream_error.WriteLineAsync($"COUNT:{counter} THREADS:{threads} TIME:{Math.Round(time / 1000, 2)}s SPEED:{Math.Round((double)counter / (time / 1000), 2)}/s");
         }
 
         static async Task StressLoop(CancellationToken ct, string url, string body, string? soap_action)
         {
             var registered = false;
+            int myname = 0;
             while (!ct.IsCancellationRequested)
             {
-                if (!registered) { Interlocked.Increment(ref threads); registered = true; }
-                await WebServiceCall(url, body, soap_action);
+                if (!registered) { myname = Interlocked.Increment(ref threads); registered = true; }
+                queue_log.Enqueue($"{myname.ToString("X4")} TX {body}");
+                var response = await WebServiceCall(url, body, soap_action);
+                queue_log.Enqueue($"{myname.ToString("X4")} RX {response}");
                 Interlocked.Increment(ref counter);
                 await Task.Delay(1);
             }
@@ -49,10 +59,24 @@ namespace stresser
 
         static async Task StressStop(CancellationTokenSource cts, int delay)
         {
-            Console.WriteLine("START!");
             await Task.Delay(delay * 1000);
-            Console.WriteLine("STOP!");
             cts.Cancel();
+        }
+
+        static async Task StressLog(CancellationToken ct)
+        {
+            using var stream_writer = new StreamWriter(Console.OpenStandardOutput(), new UTF8Encoding(false));
+            while ((!ct.IsCancellationRequested) || (queue_log.Count > 0))
+            {
+                if (queue_log.TryDequeue(out string? linha_log))
+                {
+                    await stream_writer.WriteLineAsync(linha_log);
+                }
+                else
+                {
+                    await Task.Delay(10);
+                }
+            }
         }
 
         static async Task<string> WebServiceCall(string str_url, string str_body, string? soap_action = null)
